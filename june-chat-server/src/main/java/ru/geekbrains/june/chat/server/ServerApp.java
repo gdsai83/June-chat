041dbcb0dd7106fec684f36c87e104e91c6f965b
GDSAI83 <gds.ai.83@gmail.com>
2021-06-17 13:00:03 +0300

    Create

---
+++ june-chat-server/src/main/java/ru/geekbrains/june/chat/server/ServerApp.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.june.chat.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8189);
+            System.out.println("Сервер запущен. Ожидаем подключение клиентов..");
+
+            Socket socket = serverSocket.accept();
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            System.out.println("Клиент подключился");
+            while (true) {
+                String inputMessage = in.readUTF();
+                System.out.println(inputMessage);
+                out.writeUTF("ECHO: " + inputMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
